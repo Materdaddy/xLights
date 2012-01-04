cd933429071bbf13fa6f0a982784fda19c393105
dowdybrown <dowdybrown@ad4834c4-2333-46e5-bf01-adba983f9b38>
2012-01-04 06:46:34 +0000

    Fixed sizing issue in xTester's RGB panel. Updated copyright notice to include 2012. This will be release 2012a.
    git-svn-id: svn://svn.code.sf.net/p/xlights/code/trunk@76 ad4834c4-2333-46e5-bf01-adba983f9b38

--- include/globals.h
+++ include/globals.h
@@ -20,7 +20,7 @@
 
 #define XLIGHTS_DESCRIPTION "Use xLights to control you holiday light display"
 
-#define XLIGHTS_LICENSE "Copyright 2010, 2011 Matt Brown and Paul Scala.\n\nThis file is part of xLights.\n\nxLights is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. xLights is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with xLights.  If not, see <http://www.gnu.org/licenses/>."
+#define XLIGHTS_LICENSE "Copyright 2010-2012 Matt Brown and Paul Scala.\n\nThis file is part of xLights.\n\nxLights is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. xLights is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with xLights.  If not, see <http://www.gnu.org/licenses/>."
 
 
 #endif
